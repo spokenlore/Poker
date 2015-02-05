bc30320dcd3754b6e22fe0f4904b502c93d74886
Samson <spokenlore@gmail.com>
2015-02-05 13:03:00 -0500

    Version .5
    Added percentages to combinations output file

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+# github.com/spokenlore
+# Version .5
+# February 5, 2015
+
 from generate import *
 from check import *
 from analyze import *
@@ -6,7 +10,7 @@ import random, io, os, operator
 
 def main():
 	# Generates samples using filename, and number of hands
-	generateSamples("sampleHands.txt", 10000000)
+	generateSamples("sampleHands.txt", 100000)
 	# Takes inputfile of sample hands, and outputs a file with the 'winning' hand of those cards
 	winningHands("sampleHands.txt", "winningHands.txt")
 	# Outputs combinations of each type of hand to a target outputfile
