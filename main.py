51c1d4df82438fc6e19f3b531701d79c53832bfd
Samson <spokenlore@gmail.com>
2015-02-05 22:27:27 -0500

    Version .51
    Reverting Changes

--- main.py
+++ main.py
@@ -6,9 +6,10 @@ from generate import *
 from check import *
 from analyze import *
 
-import random, io, os, operator
+import random, io, os, operator, time
 
 def main():
+	start = time.time()
 	# Generates samples using filename, and number of hands
 	generateSamples("sampleHands.txt", 100000)
 	# Takes inputfile of sample hands, and outputs a file with the 'winning' hand of those cards
@@ -17,6 +18,8 @@ def main():
 	displayCombinations("combinations.txt")
 	# Utilizes inputfile of winninghands, counts occurrences each type of hand
 	analyzeOccurences("winningHands.txt", "analysis.txt")
+
+	print "This program took %s seconds" % (time.time() - start)
 
 if __name__ == '__main__':
 	main()
